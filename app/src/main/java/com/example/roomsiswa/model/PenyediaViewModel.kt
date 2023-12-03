57053a6867381f8c85463292275b45b2f0cf43f2
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-12-03 22:08:10 +0700

    Inisialisasi HomeViewModel dengan menggunakan repositoriSiswa dari ContainerApp pada kelas AplikasiSiswa.

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -2,10 +2,16 @@ package com.example.roomsiswa.model
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomsiswa.ui.theme.AplikasiSiswa
 
 object PenyediaViewModel{
-
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
     /**
  * Fungsi ekstensi query untuk objek Application dan mengembalikan sebuah instance dari AplikasiSiswa
  */
