f4ef94360c0f8b890fde13fc610e0aef8b35bbca
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-12-03 22:05:29 +0700

    Membuat fungsi ekstensi query untuk objek aplikasiSiswa

---
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.roomsiswa.model
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.roomsiswa.ui.theme.AplikasiSiswa
+
+object PenyediaViewModel{
+
+    /**
+ * Fungsi ekstensi query untuk objek Application dan mengembalikan sebuah instance dari AplikasiSiswa
+ */
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as AplikasiSiswa)
+}
