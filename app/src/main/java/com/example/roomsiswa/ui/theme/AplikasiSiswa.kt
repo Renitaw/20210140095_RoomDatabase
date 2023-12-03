ba091bf3d1d212dbe657c0c8a4c8554814e07e50
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-12-03 21:46:59 +0700

    Inisialisasi ContainerApp pada kelas AplikasiSiswa untuk manajemen dependensi aplikasi.

--- app/src/main/java/com/example/roomsiswa/ui/theme/AplikasiSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/AplikasiSiswa.kt
@@ -1,5 +1,17 @@
 package com.example.roomsiswa.ui.theme
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.roomsiswa.Repositori.ContainerApp
+import com.example.roomsiswa.Repositori.ContainerDataApp
 
+class AplikasiSiswa : Application() {
+    /**
+     * App Container instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+     */
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
