c172e36a775dd8466688749d436fb95095cef777
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-12-04 21:21:52 +0700

    Menambahkan function HostNavigasi

--- app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
@@ -12,11 +12,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.roomsiswa.R
+import com.example.roomsiswa.ui.theme.Halaman.DestinasiEntry
+import com.example.roomsiswa.ui.theme.Halaman.DestinasiHome
+import com.example.roomsiswa.ui.theme.Halaman.EntrySiswaScreen
+import com.example.roomsiswa.ui.theme.Halaman.HomeScreen
 
 @Composable
-fun SiswaApp(navController: NavController = rememberNavController()){
+fun SiswaApp(navController: NavHostController = rememberNavController()){
+    HostNavigasi(navController = navController)
 }
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -41,4 +49,23 @@ fun SiswaTopAppBar(
             }
         }
     )
+}
+
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)})
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.popBackStack()})
+        }
+
+    }
 }
